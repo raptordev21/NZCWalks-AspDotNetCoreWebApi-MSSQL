ec96da7cea8ee3a22a42234bf9c345a3a38842b9
Himanshu Choudhary <raptordev21@gmail.com>
2024-05-06 09:53:04 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "Himanshu", "Deeksha" };
+            return Ok(studentNames);
+        }
+    }
+}
